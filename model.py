67541d0407af1c794eaeba07391e5a0f285c5645
forecho <caizhenghai@gmail.com>
2014-12-02 10:03:42 +0000

    修改数据库操作文件

--- model.py
+++ model.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys, MySQLdb, time
 
-def save_post(title, author, post_created, wechat, post_content):
+
+def __conn():
     conn= MySQLdb.connect(
             host='localhost',
             port = 3306,
@@ -12,13 +13,18 @@ def save_post(title, author, post_created, wechat, post_content):
             use_unicode=1,
             charset="utf8"
         )
-    # 使用cursor()方法获取操作游标
-    cur = conn.cursor()
+    return conn
 
-    #插入一条数据
-    today = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-    cur.execute("INSERT INTO posts VALUES(NULL, %s, %s, %s, %s, %s, %s)", (title, author, wechat, post_content, post_created, today))
-
+def __close(conn, cur):
     cur.close()
     conn.commit()
     conn.close()
+
+def save_post(title, author, post_created, wechat, post_content):
+    conn = __conn();
+    # 使用cursor()方法获取操作游标
+    cur = conn.cursor()
+    #插入一条数据
+    today = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+    cur.execute("INSERT INTO posts VALUES(NULL, %s, %s, %s, %s, %s, %s)", (title, author, wechat, post_content, post_created, today))
+    __close(conn, cur);
